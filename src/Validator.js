287a661257d70ebe658dc76fdbfa1abda2a01da3
Vyacheslav <slavik.kurganov.26@mail.ru>
2025-02-08 14:57:44 +0300

    corrected the conditions of the problem and added comments

--- src/Validator.js
+++ src/Validator.js
@@ -1,5 +1,5 @@
 export default class Validator {
   static validateUsername(username) {
-    return /^[a-zA-Z](?!.*\d{4,})[a-zA-Z0-9-_]*[a-zA-Z0-9]$/.test(username);
+    return /^[a-zA-Z](?!.*\d{4,})[a-zA-Z0-9-_]*[a-zA-Z]$/.test(username);
   }
 }
